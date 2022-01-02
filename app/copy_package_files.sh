f6f070ff6704c5790b1b31112efbdce7a893f0b0
Joseph Eagar <joeedh@gmail.com>
2022-01-01 16:35:31 -0800

    Pull one liner change to build script from gh-pages

--- app/copy_package_files.sh
+++ app/copy_package_files.sh
@@ -9,3 +9,4 @@ cp -r assets core editors electron_base path.ux pattern patterns util webgl pack
 cp main.html screen.js entry_point.js $PACKAGE_DIR
 
 rm $PACKAGE_DIR/path.ux/.git
+rm $PACKAGE_DIR/path.ux/scripts/path-controller/.git
