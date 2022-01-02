1dd6267fdeebc8d3bbcc181a069b8f9c58d92fe5
Joseph Eagar <joeedh@gmail.com>
2022-01-02 14:31:14 -0800

    merge change from gh-pages

--- app/copy_package_files.sh
+++ app/copy_package_files.sh
@@ -5,8 +5,8 @@ echo Copying Files
 mkdir package 2> /dev/null
 mkdir package/resources
 
-cp -r assets core editors electron_base path.ux pattern patterns util webgl package
+cp -r assets core editors electron_base path.ux pattern patterns util webgl $PACKAGE_DIR
 cp main.html screen.js entry_point.js $PACKAGE_DIR
 
-rm $PACKAGE_DIR/path.ux/.git
-rm $PACKAGE_DIR/path.ux/scripts/path-controller/.git
+rm -rf $PACKAGE_DIR/path.ux/.git*
+rm -rf $PACKAGE_DIR/path.ux/scripts/path-controller/.git*
