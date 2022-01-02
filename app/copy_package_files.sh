0d58369f558080eb1f84fca762a2c499abd798f6
Joseph Eagar <joeedh@gmail.com>
2022-01-02 14:22:10 -0800

    update gh-pages

--- app/copy_package_files.sh
+++ app/copy_package_files.sh
@@ -8,5 +8,5 @@ mkdir package/resources
 cp -r assets core editors electron_base path.ux pattern patterns util webgl $PACKAGE_DIR
 cp main.html screen.js entry_point.js $PACKAGE_DIR
 
-rm $PACKAGE_DIR/path.ux/.git
-rm $PACKAGE_DIR/path.ux/scripts/path-controller/.git
+rm -rf $PACKAGE_DIR/path.ux/.git*
+rm -rf $PACKAGE_DIR/path.ux/scripts/path-controller/.git*
