cdbb181a6c288a0307a078d8861680207e5e0c95
Joseph Eagar <joeedh@gmail.com>
2022-01-05 21:27:12 -0800

    bleh

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -3,10 +3,11 @@
 git commit -a
 git push
 
-git checkout gh-pages
+git checkout gh-pages || exit 1
+
 git merge master -m "merge"
 ./package.sh
 git commit -a -m "update gh-pages"
 git push
-git checkout master
+git checkout master --force
 
